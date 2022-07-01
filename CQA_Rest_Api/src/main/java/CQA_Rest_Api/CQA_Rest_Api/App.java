9865efa87b767dcc9e12014afc9bff565e073cd4
saurabhyeggewar1998 <95201793+saurabhyeggewar1998@users.noreply.github.com>
2022-07-01 10:19:18 +0530

    Added File of SpotifyApi Automation

---
+++ CQA_Rest_Api/src/main/java/CQA_Rest_Api/CQA_Rest_Api/App.java
@@ -0,0 +1,13 @@
+package CQA_Rest_Api.CQA_Rest_Api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
